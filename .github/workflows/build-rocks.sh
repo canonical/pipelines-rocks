7dfb4fe8973ffb041364e35af418ed1d5721f74d
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-07-12 14:18:09 +0200

    [skip] todo

--- .github/workflows/build-rocks.sh
+++ .github/workflows/build-rocks.sh
@@ -1,14 +1,28 @@
-#! /usr/bin/env bash -x
+#!/usr/bin/env bash
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+set -x
+
+ROCKS_DIR=$1
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 
 # Create kfp-operators-rocks directory
 [ -d ~/kfp-operators-rocks ] || mkdir ~/kfp-operators-rocks
 
-# Loop through directories to build the rock
-for d in ./*; do
-    if [ -f "rockcraft.yaml" ]; then
-        cd d
-        rockcraft pack -v
-        mv *./rock ~/kfp-operators-rocks
-        cd -
-    fi
-done
+# Build the rock from a file or the root of this repo
+if [ -f "${ROCKS_DIR}/rockcraft.yaml" ]; then
+    cd ${ROCKS_DIR}
+    rockcraft pack -v
+    mv *.rock ~/kfp-operators-rocks/"${ROCKS_DIR}-built-at-${CURRENT_TIME}.rock"
+    cd -
+else 
+    for d in ${ROCKS_DIR}/*; do
+        if [ -f "${d}/rockcraft.yaml" ]; then
+            cd ${d}
+            rockcraft pack -v
+            mv *.rock ~/kfp-operators-rocks/
+            cd -
+        fi
+    done
+fi
--- .github/workflows/import-rocks.sh
+++ .github/workflows/import-rocks.sh
@@ -1,8 +1,12 @@
-#! /usr/bin/env bash -x
+#!/usr/bin/env bash
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
 
-for FILE in kfp-operators-rocks/; do
+set -x
+
+for FILE in ~/kfp-operators-rocks/; do
     ROCK_NAME=$(basename ${FILE} .rock)
-    sudo skopeo --insecure-policy copy oci-archive:${FILE} docker-daemon:${ROCK_NAME}:v1
+    sudo skopeo --insecure-policy copy oci-archive:${FILE} docker-daemon:${ROCK_NAME}:v0-test
     docker save ${ROCK_NAME} > ${ROCK_NAME}.tar
     microk8s ctr image import ${ROCK_NAME}.tar --digests=true
 done
