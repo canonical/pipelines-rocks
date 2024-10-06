d4e8f5cfb3228238f26ecb8a917feadb34968a43
Artem Yevsiukov <a.yevsiukov@gmail.com>
2024-10-06 20:46:12 +0300

    Added `minio` rockfile

---
+++ minio/tests/test_rock.py
@@ -0,0 +1,31 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import pytest
+import subprocess
+
+from charmed_kubeflow_chisme.rock import CheckRock
+
+
+@pytest.mark.abort_on_fail
+def test_rock():
+    """Test rock."""
+    check_rock = CheckRock("rockcraft.yaml")
+    rock_image = check_rock.get_name()
+    rock_version = check_rock.get_version()
+    LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
+
+    # assert the rock contains the expected files
+    subprocess.run(
+        [
+            "docker",
+            "run",
+            "--rm",
+            LOCAL_ROCK_IMAGE,
+            "exec",
+            "ls",
+            "-la",
+            "/usr/bin/docker-entrypoint.sh",
+        ],
+        check=True,
+    )
