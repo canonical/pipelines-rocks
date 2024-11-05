a766c0c72c89e1d5486c0d4637f346b178d38fad
Artem Yevsiukov <a.yevsiukov@gmail.com>
2024-11-05 23:51:35 +0200

    Fixes

--- driver/tests/test_rock.py
+++ driver/tests/test_rock.py
@@ -1,43 +1,26 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-#
 
 import pytest
-import random
-import string
 import subprocess
+
 from charmed_kubeflow_chisme.rock import CheckRock
 
 
-@pytest.fixture()
-def rock_test_env(tmpdir):
-    """Yields a temporary directory and random docker container name, then cleans them up after."""
-    container_name = "".join(
-        [str(i) for i in random.choices(string.ascii_lowercase, k=8)]
-    )
-    yield tmpdir, container_name
-
-    try:
-        subprocess.run(["docker", "rm", container_name])
-    except Exception:
-        pass
-    # tmpdir fixture we use here should clean up the other files for us
-
-
 @pytest.mark.abort_on_fail
-def test_rock(rock_test_env):
+def test_rock():
     """Test rock."""
-    temp_dir, container_name = rock_test_env
     check_rock = CheckRock("rockcraft.yaml")
     rock_image = check_rock.get_name()
     rock_version = check_rock.get_version()
     LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
 
+    # assert the rock contains the expected files
     subprocess.run(
         [
             "docker",
             "run",
+            "--rm",
             LOCAL_ROCK_IMAGE,
             "exec",
             "ls",
@@ -46,10 +29,12 @@ def test_rock(rock_test_env):
         ],
         check=True,
     )
+
     subprocess.run(
         [
             "docker",
             "run",
+            "--rm",
             LOCAL_ROCK_IMAGE,
             "exec",
             "ls",
